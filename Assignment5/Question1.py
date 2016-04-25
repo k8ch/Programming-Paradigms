e52cbfb38d644fa1aa3a6f3a4bab0135488da63e
Kate Chernega <k8ch@users.noreply.github.com>
2016-04-25 12:16:24 -0400

    Update Question1.py

--- Assignment5/Question1.py
+++ Assignment5/Question1.py
@@ -1,10 +1,16 @@
 #Question 1
 
 # Question1.1
+#Create the function cubeLess (x, b) which calculates the power of 3 and the remainder such that
+#b =x^3+r and returns the remainder r.
+
 def cubeless(x, b):
     return b - x ** 3
 
-#Question1.2
+# Question1.2
+# Use the function cubeLess from Question 1.1 to find all numbers with a cube smaller than a given limit by
+# creating a new function smallerCube (b) that generates the results as a list of tuples. 
+
 def smallerCube(b):
     return smallerCube2(b, 1, [])
 
@@ -15,7 +21,9 @@ def smallerCube2(b, x, l):
     l.append(tuple([x, cubeless(x, b)]))
     return smallerCube2(b, x + 1, l)
 
-#Question1.3
+# Question1.3
+# Create a helper function restSum (S) to add up all the remainders from finding all cubes up to an
+# upper limit in 2.
 
 def restSum(s):
     return helperRestSum(s, 0)
@@ -28,7 +36,9 @@ def helperRestSum(s, sum):
                 sum += y
     return sum
 
-#Question1.4
+# Question1.4
+# Write another function to add all the rest sums that are multiples of 3 within a range into a list. 
+
 def showAllRestSum(r1, r2):
     return sum(r1, r2, [])
 
