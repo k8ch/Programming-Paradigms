bc20bc38edcbb63db82d46a47126a04cb77861f9
Kate Chernega <k8ch@users.noreply.github.com>
2016-04-25 12:21:29 -0400

    Added comments

--- Assignment6/Question1.go
+++ Assignment6/Question1.go
@@ -1,7 +1,26 @@
-//Kateryna Chernega
-//7246161
-//CSI2120
-//Assignment6, Question 1
+// Question 1
+
+/* 
+Create a struct House with the following fields :
+	A slice of strings for the rooms in the house
+	A string with the name
+	A slice of struct roomSz
+The helper struct roomSz should have two fields of type float32: width and length.
+Create an interface Home with the following two methods
+	inputSqft with no arguments and no return
+	printMetric with no arguments and no return
+Implement the following global functions
+	NewHouse returning a pointer to House with the field name set to “House”, the field
+rooms set to a slice containing the strings "kitchen", "living", "dining", "main" and the field
+sizeFt set to a slice of default initialized struct roomSz of the same length than
+rooms.
+	NewHouseRooms which is the same as NewHouse but taking an argument of type slice of
+strings with other rooms, e.g., "bedroom1", "bedroom2" and the fields rooms and sizeFt
+correspondingly set.
+Implement methods of the interface Home for a pointer to House
+	The method inputSqft that allows the user to input the size of each room
+	The method printMetric that prints all the rooms in the house but in meters instead of feet 
+*/
 
 package main
 
@@ -93,7 +112,8 @@ func (house House) printMetric() {
 }
 
 //testing
-/*func main(){
+/*
+func main(){
 	fmt.Println("")
 	//to test with NewHouse
 
@@ -107,6 +127,14 @@ func (house House) printMetric() {
 	house.inputSqft()
 	house.printMetric()
 }
+*/
+
+/* 
+Create a struct Semi which embeds the struct House and has no other fields. Add the global
+functions NewSemi and NewSemiRooms should behave correspondingly to the function
+NewHouse and NewHouseRooms, respectively. The only difference should be that these
+function will set the name field in the embedded House to "Semi" instead of "House". Make
+sure to also supply the required methods that a pointer Semi will implement the interface Home.
 */
 
 type Semi struct{
@@ -132,6 +160,14 @@ func NewSemiRooms(extraRooms []string) Semi{
 	return semi
 }
 
+/*
+Write a main routine which makes a slice of type Home as follows
+homes := []Home{NewHouse(), NewSemi(),
+ NewHouseRooms([]string{"bedroom1", "bedroom2"})}
+Then loops over all homes calling inputSqft on each. Afterwards loops over all homes
+again calling printMetric. 
+
+*/
 
 func main(){
 	homes := []Home{NewHouse(), NewSemi(), NewHouseRooms([]string{"bedroom1", "bedroom2"})}
